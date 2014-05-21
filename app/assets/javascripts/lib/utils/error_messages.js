9f3d33bafe84e799c83c282c960723c4c240a682
chee <chee@snaek.org>
2014-05-21 21:01:40 +0100

    error messages cs→js

---
+++ app/assets/javascripts/lib/utils/error_messages.js
@@ -0,0 +1,15 @@
+define(function() {
+
+  "use strict";
+
+  return function() {
+    this.systemError = function() {
+      return "<div class='error--system js-error>" +
+              "<span class='error__image hazard-sign' role='img'>&#57414;</span>" +
+              "<div class='error__inner'>" +
+                  "<h3 class='error__title error--system__title'>Oops&hellip; something broke. Please try again.</h3>" +
+              "</div>" +
+            "</div>";
+    };
+  };
+});
